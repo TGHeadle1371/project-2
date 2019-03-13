ebdb99b5064fbc90292cc5642951085340c31108
TGHeadle1371 <tgheadle@gmail.com>
2019-03-13 12:23:19 -0600

    landing route added

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,9 @@
 var db = require("../models");
 
-module.exports = function (app) {
+module.exports = function(app) {
   // Load index page
-  app.get("/", function (req, res) {
-    db.Example.findAll({}).then(function (dbExamples) {
+  app.get("/", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
         msg: "Welcome!",
         examples: dbExamples
@@ -11,22 +11,31 @@ module.exports = function (app) {
     });
   });
   // Load signup page
-  app.get("/login", function (req, res) {
-    db.Example.findAll({}).then(function (dbExamples) {
+  app.get("/login", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
       res.render("login", {
         msg: "Welcome!",
         examples: dbExamples
       });
     });
   });
+  // Load signup page
+  app.get("/landing", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("landing", {
+        msg: "Welcome " + req.user.name + "!",
+        examples: dbExamples
+      });
+    });
+  });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function (req, res) {
+  app.get("/example/:id", function(req, res) {
     db.Example.findOne({
       where: {
         id: req.params.id
       }
-    }).then(function (dbExample) {
+    }).then(function(dbExample) {
       res.render("example", {
         example: dbExample
       });
@@ -34,7 +43,7 @@ module.exports = function (app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
+  app.get("*", function(req, res) {
     res.render("404");
   });
 };
