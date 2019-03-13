d16ef558bb03614983c5c5c8f63d777742fb0a4f
TGHeadle1371 <tgheadle@gmail.com>
2019-03-13 13:02:23 -0600

    Landing page/Index home page created

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -23,7 +23,7 @@ module.exports = function(app) {
   app.get("/landing", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.render("landing", {
-        msg: "Welcome " + req.user.name + "!",
+        msg: "Welcome!",
         examples: dbExamples
       });
     });
