d31b07b26d0513c0d9b5c38d038de69296ac5a1d
Armi <armi.avelino@gmail.com>
2019-03-12 22:17:37 -0600

    fix Tables

--- models/user.js
+++ models/user.js
@@ -1,42 +1,47 @@
 module.exports = function(sequelize, DataTypes) {
-    var User = sequelize.define("User", {
-      first_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
-            },
-        },	
-	last_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
-            },
-        },	
-	user_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
-            },
-        },
-	email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
-            },
-        },	
-	password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
-            },
-        },
-	desired_goal: DataTypes.STRING,	
-	image: DataTypes.STRING	
-    });
-    return User;
+  var User = sequelize.define("User", {
+    // eslint-disable-next-line camelcase
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    // eslint-disable-next-line camelcase
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    // eslint-disable-next-line camelcase
+    user_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    // eslint-disable-next-line camelcase
+    desired_goal: DataTypes.STRING,
+    // eslint-disable-next-line prettier/prettier
+	  image: DataTypes.STRING	
+  });
+  return User;
 };
